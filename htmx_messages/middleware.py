a63f075dfe87767b74d2dc7162d7048cd5806fc0
Benoit Blanchon <git@benoitblanchon.fr>
2022-10-04 18:53:07 +0200

    Don't alter the response if there are no messages

--- htmx_messages/middleware.py
+++ htmx_messages/middleware.py
@@ -11,28 +11,34 @@ class HtmxMessageMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
 
         # The HX-Request header indicates that the request was made with HTMX
-        if "HX-Request" in request.headers:
+        if "HX-Request" not in request.headers:
+            return response
 
-            # Get the existing HX-Trigger that could have been defined by the view
-            hx_trigger = response.headers.get("HX-Trigger")
+        # Extract the messages
+        messages = [
+            {"message": message.message, "tags": message.tags}
+            for message in get_messages(request)
+        ]
+        if not messages:
+            return response
 
-            if hx_trigger is None:
-                # If the HX-Trigger is not set, start with an empty object
-                hx_trigger = {}
-            elif hx_trigger.startswith("{"):
-                # If the HX-Trigger uses the object syntax, parse the object
-                hx_trigger = json.loads(hx_trigger)
-            else:
-                # If the HX-Trigger uses the string syntax, convert to the object syntax
-                hx_trigger = {hx_trigger: True}
+        # Get the existing HX-Trigger that could have been defined by the view
+        hx_trigger = response.headers.get("HX-Trigger")
 
-            # Add the messages array in the HX-Trigger object
-            hx_trigger["messages"] = [
-                {"message": message.message, "tags": message.tags}
-                for message in get_messages(request)
-            ]
+        if hx_trigger is None:
+            # If the HX-Trigger is not set, start with an empty object
+            hx_trigger = {}
+        elif hx_trigger.startswith("{"):
+            # If the HX-Trigger uses the object syntax, parse the object
+            hx_trigger = json.loads(hx_trigger)
+        else:
+            # If the HX-Trigger uses the string syntax, convert to the object syntax
+            hx_trigger = {hx_trigger: True}
 
-            # Add or update the HX-Trigger
-            response.headers["HX-Trigger"] = json.dumps(hx_trigger)
+        # Add the messages array in the HX-Trigger object
+        hx_trigger["messages"] = messages
+
+        # Add or update the HX-Trigger
+        response.headers["HX-Trigger"] = json.dumps(hx_trigger)
 
         return response
