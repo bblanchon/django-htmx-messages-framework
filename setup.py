0450b02c9ed4ad30c103e3a41a4b41e1e17f4df7
Fabian Lange <fabian.lange.berlin@gmail.com>
2023-04-03 08:22:32 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='django-htmx-messages-framework',
+    version='0.1.0',
+    packages=['htmx_messages'],
+    url='https://github.com/bblanchon/django-htmx-messages-framework',
+    author='bblanchon',
+)
